c1c92724b1239c8df2c7c306baac7e60f9303ba2
Daniel John <dj71286@gmail.com>
2023-09-06 23:07:28 +0100

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,50 +1,23 @@
 #!/usr/bin/nodejs
 
 const request = require('request');
+const movieId = process.argv[2];
 
-// Check if a movie ID is provided as a command-line argument
-if (process.argv.length !== 3) {
-  console.error('Usage: node 0-starwars_characters.js <Movie_ID>');
-  process.exit(1);
-}
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-const movieID = process.argv[2];
-
-// URL for the Star Wars API to fetch movie details
-const movieURL = `https://swapi.dev/api/films/${movieID}/`;
-
-// Function to fetch characters for a movie
-function fetchMovieCharacters (url) {
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-      process.exit(1);
-    }
-
-    if (response.statusCode !== 200) {
-      console.error('Error:', response.statusCode);
-      process.exit(1);
-    }
-
-    const movieData = JSON.parse(body);
-
-    movieData.characters.forEach((characterURL) => {
-      request(characterURL, (charError, charResponse, charBody) => {
-        if (charError) {
-          console.error('Error:', charError);
-          process.exit(1);
+request.get(url, (error, response, body) => {
+  if (error) console.log(error);
+  if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const characterName = JSON.parse(body).name;
+          console.log(characterName);
         }
-
-        if (charResponse.statusCode !== 200) {
-          console.error('Error:', charResponse.statusCode);
-          process.exit(1);
-        }
-
-        const characterData = JSON.parse(charBody);
-        console.log(characterData.name);
       });
-    });
-  });
-}
-
-fetchMovieCharacters(movieURL);
+    }
+  }
+});
