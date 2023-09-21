e8109bd1204e7142636055ea555a2408bd145540
Nnaemeka Daniel John <113605239+dukeofhazardz@users.noreply.github.com>
2023-09-21 21:59:28 +0100

    Fixed pycodestyle error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,15 +8,16 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    
-    # Create a list to store the minimum number of coins needed for each amount from 0 to 'total'
+
+    # Create a list to store the minimum number of coins needed
+    # for each amount from 0 to 'total'
     dp = [float('inf')] * (total + 1)
     dp[0] = 0  # Zero coins are needed to make change for 0
-    
+
     for coin in coins:
         for amount in range(coin, total + 1):
             dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    
+
     if dp[total] == float('inf'):
         return -1  # Cannot make change for the given total
     else:
